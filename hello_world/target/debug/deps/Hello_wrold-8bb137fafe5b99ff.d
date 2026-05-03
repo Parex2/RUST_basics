c08d63efc5438ec9334d5fb6b9e435ab79d889a1
Marcin <mpianka1234@gmail.com>
2026-05-03 06:28:18 +0200

    First commit

---
+++ hello_world/target/debug/deps/Hello_wrold-8bb137fafe5b99ff.d
@@ -0,0 +1,5 @@
+C:\Projekty\RUST\hello_world\target\debug\deps\Hello_wrold-8bb137fafe5b99ff.d: src\main.rs
+
+C:\Projekty\RUST\hello_world\target\debug\deps\libHello_wrold-8bb137fafe5b99ff.rmeta: src\main.rs
+
+src\main.rs:
